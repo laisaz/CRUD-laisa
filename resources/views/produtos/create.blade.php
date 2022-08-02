5d801ed9a844dadac2f29ef8214cf57471ea6124
laisa <laisa.alvarenga0702@hotmail.com>
2022-08-02 09:54:40 -0300

    primeira commit

---
+++ resources/views/produtos/create.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device=width, initial-sacale=1.0">
+    <meta http-equiv="X-UA-Compartible" content="ie-edge">
+    <title>Cadastrar um novo produto</title>
+</head>
+<body>
+    <form action="{{ route('registrar_produto')}}" method="POST">
+        <?php echo csrf_field() ?>
+    
+        <label for="">Nome</label> <br />
+        <input type="text" name="nome"> <br />
+
+        <label for="">Custo</label> <br />
+        <input type="text" name="custo"> <br />
+
+        <label for="">Preco</label> <br />
+        <input type="text" name="preco"> <br />
+
+        <label for="">Quantidade</label> <br />
+        <input type="text" name="quantidade"> <br />
+
+        <button>Salvar</button>
+    </form>
+</body>
+</html><?php
